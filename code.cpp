fcf8ef83a5abde51286c841301fd7ba6531f5b18
websitedevlopment <88392033+websitedevlopment@users.noreply.github.com>
2022-11-28 20:49:18 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,46 @@
+ #include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long 
+#define mod 1000000007
+#define db1(x)  cerr <<#x<<"="<<x<<'\n'
+#define db2(x,y)  cerr <<#x<<"="<<x<<" , "<<#y<<"="<<y<<'\n'
+#define db3(x,y,z)  cerr <<#x<<"="<<x<<" , "<<#y<<"="<<y<<" , "<<#z<<"="<<z<<'\n'
+int main()
+{
+ ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+   #ifndef ONLINE_JUDGE
+           freopen("input","r",stdin);
+           freopen("output","w",stdout);
+   #endif
+           int i=0;
+           while(i++<9){
+
+string x="//";
+char y='"';
+      cout<<"<iframe src="<<y<<"http:"<<x;
+      cout<< "results.ietdavv.edu.in/DisplayStudentResult?rollno=";
+      // 20C2116
+      string ans="19C5"; cout<<ans;
+      cout<<1<<0<<i;
+      // cout<<endl;
+      cout<<"&typeOfStudent=Regular"<<'"'<<" "<<  "height="<<'"'<<800<<y<< "width="<<y<<1000<<y<<"></iframe>";
+      cout<<endl;
+           }
+             while(i++<70){
+
+string x="//";
+char y='"';
+      cout<<"<iframe src="<<y<<"http:"<<x;
+      cout<< "results.ietdavv.edu.in/DisplayStudentResult?rollno=";
+      // 20C2116
+      string ans="19C5"; cout<<ans;
+      cout<<1<<i;
+      // cout<<endl;
+      cout<<"&typeOfStudent=Regular"<<'"'<<" "<<  "height="<<'"'<<800<<y<< "width="<<y<<1000<<y<<"></iframe>";
+      cout<<endl;
+           }
+       return 0;
+}
